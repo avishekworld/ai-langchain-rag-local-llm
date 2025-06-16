c9259e8f0390ca71d156dc737db047043ba14ef4
Avi <engg.avishek@gmail.com>
2025-06-15 22:29:54 -0700

    Create ai langchain based rag application with local llm

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import os
+from PyPDF2 import PdfReader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+
+def extract_text_from_pdf(pdf_path):
+    reader = PdfReader(pdf_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+def create_vector_store(text, path="faiss_index"):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_text(text)
+    
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_store = FAISS.from_texts(chunks, embeddings)
+    vector_store.save_local(path)
+    
+    return vector_store
+
+def load_vector_store(path="faiss_index"):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_store = FAISS.load_local(path, 
+                                    embeddings, 
+                                    allow_dangerous_deserialization=True)
+    return vector_store
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Langchain RAG AI Application
+
+
+This AI app allow user to ask question and get answers on their uploaded PDF using local running LLM model. 
