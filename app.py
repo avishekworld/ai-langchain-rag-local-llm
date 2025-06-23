325bc7c46235a172ee99f1b78b352ee392339b3f
Avi <engg.avishek@gmail.com>
2025-06-22 20:22:47 -0700

    Add AI RAG App lang chain and ui

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from PyPDF2 import PdfReader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import Ollama
 from langchain.chains import RetrievalQA
+from langchain.chains.question_answering import load_qa_chain
 
 def extract_text_from_pdf(pdf_path):
     reader = PdfReader(pdf_path)
@@ -29,3 +31,47 @@ def load_vector_store(path="faiss_index"):
                                     embeddings, 
                                     allow_dangerous_deserialization=True)
     return vector_store
+
+def build_rag_chain(path="faiss_index"):
+    vector_store = load_vector_store(path)
+    retriever = vector_store.as_retriever()
+    # Build the QA chain
+    llm = Ollama(model="llama3.2:3B")
+    qa_chain = load_qa_chain(llm, chain_type="stuff")
+    rag = RetrievalQA(retriever=retriever, combine_documents_chain=qa_chain)
+    return rag
+
+st.title("AI RAG app with LangChain and Llama")
+
+st.write("Upload a PDF file and ask questions about its content.")
+
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+if uploaded_file is not None:
+    pdf_path = f"uploaded/{uploaded_file.name}"
+    os.makedirs("uploaded", exist_ok=True)
+
+    with open(pdf_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.success("File uploaded successfully!")
+
+    text = extract_text_from_pdf(pdf_path)
+
+    st.info("Creating vector store...")
+    
+    create_vector_store(text)
+
+    st.info("Iniitializing AI RAG App...")
+
+    rag = build_rag_chain()
+
+    st.success("AI RAG App is ready!")
+
+    if 'rag' in locals():
+        question = st.text_input("Ask a question about the uploaded PDF:")
+        
+        if question:
+            with st.spinner("Getting answer..."):
+                answer = rag.run(question)
+                st.success(f"Answer: {answer}")
--- readme.md
+++ readme.md
@@ -1,4 +1,14 @@
 # Langchain RAG AI Application
 
 
-This AI app allow user to ask question and get answers on their uploaded PDF using local running LLM model. 
+This AI RAG app allow user to ask question and get answers on their uploaded PDF using local running LLM model. 
+
+## Instruction
+
+Execute following command to the run the AI RAG app:
+
+streamlit run app.py
+
+## Screenshot
+
+![AI RAG App](ai_rag_app.png)
